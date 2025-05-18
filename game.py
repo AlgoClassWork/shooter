ed575c7bed66e423f7dd13503d52a61ad78ef411
AlgoClassWork <kyxec.play@gmail.com>
2025-05-18 12:22:52 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,55 +1,95 @@
 from pygame import *
+
 # Параметры игры
-WINDOW_WIDTH = 700
-WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 700  # Ширина окна
+WINDOW_HEIGHT = 500  # Высота окна
 
-IMG_BACKGROUND = 'фон.png'
-IMG_PLAYER = 'ракета.png'
-IMG_BULLET = 'пуля.png'
-IMG_ENEMY = 'враг.png'
+# Пути к изображениям
+IMG_BACKGROUND = 'фон.png'  # Фон игры
+IMG_PLAYER = 'ракета.png'  # Игрок (ракета)
+IMG_BULLET = 'пуля.png'  # Пуля
+IMG_ENEMY = 'враг.png'  # Враг
 
-score = 0
-lost = 0
-goal = 10
+# Игровые переменные
+score = 0  # Счёт игрока
+lost = 0  # Количество потерянных врагов
+goal = 10  # Цель игры (уничтожить 10 врагов)
 
-finish = False
+finish = False  # Флаг завершения игры
 
 # Классы для настройки персонажей
+
 class GameSprite(sprite.Sprite):
+    """Класс для общего поведения спрайтов (игроков, врагов, пуль)"""
+    
     def __init__(self, img, width, height, x, y, speed):
-        super().__init__()
-        self.image = transform.scale(image.load(img), (width, height))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.speed = speed
+        """
+        Инициализация спрайта.
+        :param img: Путь к изображению
+        :param width: Ширина спрайта
+        :param height: Высота спрайта
+        :param x: Начальная позиция по оси X
+        :param y: Начальная позиция по оси Y
+        :param speed: Скорость движения
+        """
+        super().__init__()  # Инициализация родительского класса
+        self.image = transform.scale(image.load(img), (width, height))  # Загрузка и масштабирование изображения
+        self.rect = self.image.get_rect()  # Получаем прямоугольник для позиционирования
+        self.rect.x = x  # Устанавливаем начальную позицию по X
+        self.rect.y = y  # Устанавливаем начальную позицию по Y
+        self.speed = speed  # Устанавливаем скорость
 
     def show(self):
+        """Отображение спрайта на экране"""
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-player = GameSprite(IMG_PLAYER, 100, 100, 300, 400, 5)
-# Работа с экраном
-window = display.set_mode(( WINDOW_WIDTH, WINDOW_HEIGHT ))
-display.set_caption('Шутер')
 
-background =  transform.scale(image.load(IMG_BACKGROUND), (WINDOW_WIDTH, WINDOW_HEIGHT))
+class Player(GameSprite):
+    """Класс для игрока"""
+    
+    def update(self):
+        """
+        Обновление позиции игрока в зависимости от нажатых клавиш.
+        Игрок двигается влево и вправо.
+        """
+        keys = key.get_pressed()  # Получаем состояние клавиш
+        if keys[K_a] and self.rect.x > 0:  # Если нажата клавиша 'A' и не выходит за пределы
+            self.rect.x -= self.speed  # Двигаем влево
+        if keys[K_d] and self.rect.x < 600:  # Если нажата клавиша 'D' и не выходит за пределы
+            self.rect.x += self.speed  # Двигаем вправо
+
+
+# Создание объекта игрока
+player = Player(IMG_PLAYER, 100, 100, 300, 400, 5)
+
+# Настройка экрана игры
+window = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))  # Устанавливаем размеры окна
+display.set_caption('Шутер')  # Устанавливаем заголовок окна
+
+# Загрузка фона
+background = transform.scale(image.load(IMG_BACKGROUND), (WINDOW_WIDTH, WINDOW_HEIGHT))
+
 # Работа с текстом
-font.init()
-font1 = font.Font('Шрифт.ttf', 100)
-font2 = font.Font('Шрифт.ttf', 50)
+font.init()  # Инициализация шрифтов
+font1 = font.Font('Шрифт.ttf', 100)  # Шрифт для большого текста
+font2 = font.Font('Шрифт.ttf', 50)  # Шрифт для текста среднего размера
 
 # Игровой цикл
-clock = time.Clock()
+clock = time.Clock()  # Настройка таймера для FPS
 
 while True:
     # Обработка событий
     for some_event in event.get():
-        if some_event.type == QUIT:
-            exit()
+        if some_event.type == QUIT:  # Если событие закрытия окна
+            exit()  # Закрытие программы
 
-    # Отображение обьектов
-    window.blit(background, (0, 0))
-    player.show()
-    
-    display.update()
-    clock.tick(100)
+    # Отображение объектов
+    window.blit(background, (0, 0))  # Отображаем фон
+    player.show()  # Отображаем игрока
+
+    # Обновление позиции объектов
+    player.update()  # Обновляем позицию игрока
+
+    # Обновление экрана
+    display.update()  # Обновляем экран
+    clock.tick(100)  # Ограничиваем FPS до 100
